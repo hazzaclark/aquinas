cc9faad1bef032c70722851a219d79241df3d30c
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-08-25 17:38:05 +0100

    wip memory mapping test
    in a similar vein to lib68k_mem

--- src/main.cc
+++ src/main.cc
@@ -3,12 +3,32 @@
 
 // THIS FILE PERTAINS TOWARDS THE MAIN FUNCTIONALITY OF THE PROGRAM
 
+// NESTED INCLUDES
+
+#include <mmu_mem.hh>
+#include <mmu_def.hh>
+
 // SYSTEM INCLUDES
 
 #include <cstdio>
 
+using namespace aquinas::mmu_mem_opts;
+using namespace aquinas::mmu_mem;
+
 int main(void)
 {
-    std::printf("HARRY CLARK - M68851 MMU SIMULATOR\n");
+    printf("==================================\n");
+    printf("HARRY CLARK - M68851 MMU SIMULATOR\n");
+    printf("===================================\n");
+
+    MEMORY_MANAGER MEM_MANAGER;
+
+    MEM_MANAGER.MAP_MEMORY(0x00000, 0x7FFFF, true);
+    MEM_MANAGER.MAP_MEMORY(0x80000, 0xFFFFF, false);
+
+    MEM_MANAGER.SHOW_MEMORY_MAPS();
+
+    MEM_MANAGER.SET_STOPPED(true);
+    MEM_MANAGER.SHOW_MEMORY_MAPS();
     return 0;
 }
