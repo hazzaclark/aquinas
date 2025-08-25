4977b3b8000bd0fc4df5031333fc381d8c08f55e
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-08-25 17:07:47 +0100

    what a ballache

--- src/mmu_util.cc
+++ src/mmu_util.cc
@@ -13,17 +13,38 @@
 #include <mmu_def.hh>
 #include <util.hh>
 
+// SYSTEM INCLUDES
+
+#include <cstdarg>
+#include <iostream>
+
 using namespace aquinas;
 using namespace aquinas::mmu_mem;
 using namespace aquinas::mmu_mem_opts;
 
-void MEMORY_MANAGER::MEM_TRACE(MEMORY_OPTION OP, U32 ADDRESS, 
-                            MEMORY_SIZE SIZE, U32 VALUE) const
+void MEM_TRACE(MEMORY_OPTION OP, U32 ADDRESS, 
+                            MEMORY_SIZE SIZE, U32 VALUE)
 {
 
 }
 
-void MEMORY_MANAGER::VERBOSE_TRACE(const std::string& MSG) const
+// THIS IS THE BEST WAY I HAVE BEEN ABLE TO REPLICATE THE __VA_ARGS__ FROM C
+// USING C++17 
+
+// IT DOES LOOK REALLY MESSY, SURE - BUT IT WAS BETTER THAN WHAT I HAD BEFOREHAND
+// WHEREBY I WAS CONVERTING EVERY ARG INTO TO_STRING
+
+void MEMORY_MANAGER::VERBOSE_TRACE(const char* FMT, ...) const
 {
-    
+    if(IS_TRACE_ENABLED(MEMORY_OPT_FLAG::VERBOSE))
+    {
+        va_list ARGS;
+        va_start(ARGS, FMT);
+        
+        std::printf("[VERBOSE] ");
+        std::vprintf(FMT, ARGS);
+        std::printf("\n");
+        
+        va_end(ARGS);
+    }
 }
