1f0c6ab2dd47a535bd042e0d29bd6a6ad1d93a5e
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-08-25 14:45:55 +0100

    modularised between namespaces

--- inc/mmu_mem.hh
+++ inc/mmu_mem.hh
@@ -4,6 +4,10 @@
 // THIS FILE PERTAINS TOWARDS THE FUNCTIONALITY ENCOMPASSING MEMORY READ AND WRITES
 // UTILISING INTRINSIC C++ FEATURES TO DYNAMICALLY HELP WITH MEMORY OPERATIONS
 
+// MOREOVER, THIS IS ESSENTIALLY A C++ PORT OF LIB68K_MEM - WHICH IS BEING REPURPOSED
+// FOR THE SOLE BENEFIT OF REPRODUCING THE SAME EFFICIENT READ AND WRITES AS PER THE 68K BUS
+// ALBEIT WITH SLIGHT ALTERATIONS TO ACCOMMODATE FOR THE INTRINSIC'S OF THE MMU
+
 #ifndef MMU_MEM_HH
 #define MMU_MEM_HH
 
@@ -16,10 +20,11 @@
 
 #include <array>
 #include <algorithm>
+#include <memory>
 
 namespace aquinas
 {
-    namespace mmu_mem
+    namespace mmu_mem_opts
     {
         #define     MAX_BUFFERS             5
         #define     MAX_ADDR_START          0x0000000
@@ -67,6 +72,30 @@ namespace aquinas
         {
             BASIC = 1 << 0,
             VERBOSE = 1 << 1,
+        };
+    }
+
+    namespace mmu_mem
+    {
+        struct MEMORY_USAGE
+        {
+            U32 READ_COUNT;
+            U32 WRITE_COUNT;
+            U32 LAST_READ;
+            U32 LAST_WRITE;
+            U32 VIOLATION;
+            bool ACCESSED;
+        };
+
+        class MEMORY_BUFFER
+        {
+            private:
+                U32 BASE;
+                U32 END;
+                U32 SIZE;
+                std::unique_ptr<U8[]> BUFFER;
+                bool WRITEABLE;
+                MEMORY_USAGE USAGE;
         };
     }
 }
