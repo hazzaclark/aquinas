2eab764b7bea08b333b607e3f126139fe81e11b9
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-08-26 21:50:17 +0100

    added same but for opcode look table

--- inc/mmu.hh
+++ inc/mmu.hh
@@ -24,8 +24,27 @@ namespace aquinas
         
         // CREATE A HANDLER TYPE FOR BEING ABLE TO DELEGATE THE FORWARD
         // DECLARATION OF THE MMU AGAINST COMMON PARAMS
+
         template<typename MMU_EA_READER>
         using MMU_HANDLER = std::function<void(MMU_BASE*, U32 EA, U16 MASK, MMU_EA_READER READ_EA);
+
+        // CREATE A HANDLER TYPE FOR THE OPCODE HANDLER STRUCTURE
+        // WHICH WILL PRESUPPOSE THE BASELINE MMU HANDLER TO DETERMINE EA
+        // IN CONJUNCTION WITH THE MASK, MATCH AND CYCLES
+
+        template<typename MMU_EA_READER>
+        struct MMU_OPCODE
+        {
+            MMU_HANDLER<MMU_EA_READER> HANDLER;
+            U16 MASK;
+            U16 MATCH;
+            U8 CYCLES;
+            const char* NAME;
+
+            MMU_OPCODE() = default;
+            MMU_OPCODE(MMU_HANDLER<MMU_EA_READER> HANDLER_, U16 MASK_, U16 MATCH_, U8 CYCLES_, const char* NAME_)
+                : HANDLER(HANDLER_), MASK(MASK_), MATCH(MATCH_), CYCLES(CYCLES_), NAME(NAME_) {} 
+        };
     }
 }
 
