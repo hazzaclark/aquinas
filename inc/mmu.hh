956e267562ebf251a7aa1eb243009d86c2cd8cf8
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-08-26 21:40:08 +0100

    wip mmu modularisation
    going to see how templates will fair when it comes to create types for handling opcodes

--- inc/mmu.hh
+++ inc/mmu.hh
@@ -9,12 +9,23 @@
 // NESTED INCLUDES
 
 #include <mmu_def.hh>
+#include <mmu_mem.hh>
+#include <util.hh>
+
+// SYSTEM INCLUDES
+
+#include <functional>
 
 namespace aquinas
 {
     namespace mmu
     {
+        class MMU_BASE; 
         
+        // CREATE A HANDLER TYPE FOR BEING ABLE TO DELEGATE THE FORWARD
+        // DECLARATION OF THE MMU AGAINST COMMON PARAMS
+        template<typename MMU_EA_READER>
+        using MMU_HANDLER = std::function<void(MMU_BASE*, U32 EA, U16 MASK, MMU_EA_READER READ_EA);
     }
 }
 
