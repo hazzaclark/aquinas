16a4f4ce921a23aad85a41d0e0e11009f908f920
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-08-29 01:50:27 +0100

    templating was in fact not worth
    we can always just presuppose the functionality of templates by creating a using pragmatic
    
    this propagates similar results either way and doesn't force us into messy arbitrary template class definitions at the bottom of source impls

--- inc/mmu.hh
+++ inc/mmu.hh
@@ -22,67 +22,36 @@ namespace aquinas
 {
     namespace mmu
     {
+        // RAW FUNCTION POINTER FOR MEMORY READ
+        // MITIGATE THE OVERHEAD FOR NEEDLESS TEMPLATE CREATION
+
+        using MMU_FUNC_READ_16 = std::function<U16(U32)>;
+
         class MMU_BASE; 
         
         // CREATE A HANDLER TYPE FOR BEING ABLE TO DELEGATE THE FORWARD
         // DECLARATION OF THE MMU AGAINST COMMON PARAMS
 
-        template<typename MMU_EA_READER>
-        using MMU_HANDLER = std::function<void(MMU_BASE*, U32 EA, U16 MASK, MMU_EA_READER READ_EA)>;
+        using MMU_HANDLER = std::function<void(MMU_BASE*, MMU_FUNC_READ_16, U32)>;
 
         // CREATE A HANDLER TYPE FOR THE OPCODE HANDLER STRUCTURE
         // WHICH WILL PRESUPPOSE THE BASELINE MMU HANDLER TO DETERMINE EA
         // IN CONJUNCTION WITH THE MASK, MATCH AND CYCLES
 
-        template<typename MMU_EA_READER>
         struct MMU_OPCODE
         {
-            MMU_HANDLER<MMU_EA_READER> HANDLER;
+            MMU_HANDLER HANDLER;
             U16 MASK;
             U16 MATCH;
             U8 CYCLES;
             const char* NAME;
-
-            MMU_OPCODE() = default;
-            MMU_OPCODE(MMU_HANDLER<MMU_EA_READER> HANDLER_, U16 MASK_, U16 MATCH_, U8 CYCLES_, const char* NAME_)
-                : HANDLER(HANDLER_), MASK(MASK_), MATCH(MATCH_), CYCLES(CYCLES_), NAME(NAME_) {} 
         };
 
         class MMU_BASE
         {
             public:
-                template<typename MMU_EA_READER>
-                using MMU_HANDLER_TABLE = std::array<MMU_HANDLER<MMU_EA_HANDLER>, 0x10000>;
-
-                using MMU_CYCLES_TABLE = std::array<U8, 0x10000>;
-
                 MMU_BASE();
                 ~MMU_BASE() = default;
-
-                // OPCODE HANDLER DECLARATIONS
-                template<typename MMU_EA_READER>
-                static void PFLUSHA_HANDLER(MMU_BASE* MMU, U32 EA, U16 MASK, MMU_EA_READER EA_);
-
-                template<typename MMU_EA_READER>
-                static void PFLUSHAN_HANDLER(MMU_BASE* MMU, U32 EA, U16 MASK, MMU_EA_READER EA_);
-
-                template<typename MMU_EA_READER>
-                static void PFLUSH_HANDLER(MMU_BASE* MMU, U32 EA, U16 MASK, MMU_EA_READER EA_);
-
-                template<typename MMU_EA_READER>
-                static void PLOADR_HANDLER(MMU_BASE* MMU, U32 EA, U16 MASK, MMU_EA_READER EA_);
-
-                template<typename MMU_EA_READER>
-                static void PLOADW_HANDLER(MMU_BASE* MMU, U32 EA, U16 MASK, MMU_EA_READER EA_);
-
-                template<typename MMU_EA_READER>
-                static void PMOVE_HANDLER(MMU_BASE* MMU, U32 EA, U16 MASK, MMU_EA_READER EA_);
-
-                template<typename MMU_EA_READER>
-                static void PTESTR_HANDLER(MMU_BASE* MMU, U32 EA, U16 MASK, MMU_EA_READER EA_);
-
-                template<typename MMU_EA_READER>
-                static void PTESTW_HANDLER(MMU_BASE* MMU, U32 EA, U16 MASK, MMU_EA_READER EA_);
 
             private:
                 U32 CRP;
